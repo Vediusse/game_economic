e0bfd857eb3b9512599baa77128d18fa091b3906
IrishMan05 <grigodssf65@gmail.com>
2023-11-11 22:33:47 +0300

    Первый таск: изменена клавиатура и текст сообщения для команды start

--- keyboard/start.py
+++ keyboard/start.py
@@ -5,20 +5,7 @@ def user_game_keyboard() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [
-                InlineKeyboardButton(text="✝️", callback_data=f"charity_btns "),
-                InlineKeyboardButton(text="💰", callback_data=f"charity_btns "),
-                InlineKeyboardButton(text="🤷", callback_data=f"charity_btns"),
-                InlineKeyboardButton(text="⚔️", callback_data=f"charity_btns "),
-            ],
-            [
-                InlineKeyboardButton(text="500", callback_data=f"charity_btns"),
-                InlineKeyboardButton(text="647", callback_data=f"charity_btns"),
-                InlineKeyboardButton(text="298", callback_data=f"charity_btns"),
-                InlineKeyboardButton(text="120", callback_data=f"charity_btns"),
-            ],
-            [
-                InlineKeyboardButton(text="Согласиться", callback_data=f"charity_btns"),
-                InlineKeyboardButton(text="Отказаться", callback_data=f"charity_btns"),
-            ],
+                InlineKeyboardButton(text="Далее", callback_data=f"start_button "),
+            ]
         ]
     )
